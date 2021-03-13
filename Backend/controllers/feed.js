f826ffb2fe51766d29f6496c01cf9bae7ffce826
Prateek9144 <prateekchouhan00@gmail.com>
2021-03-13 17:11:15 +0530

    post action completed

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const Post = require("../models/post");
 const { validationResult } = require("express-validator");
 
@@ -92,4 +95,93 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(res);
+  if (!errors.isEmpty()) {
+    const error = new Error("Post not found.");
+    error.statusCode = 404;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  console.log("old", imageUrl);
+  if (req.file) {
+    imageUrl = req.file.path;
+    console.log("new", imageUrl);
+  }
+  console.log(imageUrl);
+  imageUrl = imageUrl.replace(/\\/g, "/");
+  if (!imageUrl) {
+    const error = new Error();
+    error.message = "No image picked.";
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Post not found!");
+        error.statusCode = 404;
+        // console.log(error);
+        throw error;
+      }
+      post.title = title;
+      post.content = content;
+      if (post.imageUrl !== imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then((result) => {
+      return res.json({
+        message: "Post updated.",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!errors.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  let imageUrl;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Post not found!");
+        error.statusCode = 404;
+        // console.log(error);
+        throw error;
+      }
+      imageUrl = post.imageUrl;
+      return Post.deleteOne({ _id: postId }).then((result) => {
+        clearImage(imageUrl);
+        return res.status(200).json({
+          message: "Post delteed successful",
+          result: result,
+        });
+      });
+    })
+    .catch((err) => {
+      if (!errors.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (imagePath) => {
+  const filePath = path.join(__dirname, "..", imagePath);
+  fs.unlink(filePath, (err) => {
+    console.log(err);
+  });
 };
--- Frontend/src/pages/Feed/Feed.js
+++ Frontend/src/pages/Feed/Feed.js
@@ -50,9 +50,7 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch("http://localhost:8080/feed/posts", {
-      method: "GET",
-    })
+    fetch("http://localhost:8080/feed/posts")
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch posts.");
@@ -61,7 +59,12 @@ class Feed extends Component {
       })
       .then((resData) => {
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map((post) => {
+            return {
+              ...post,
+              imagePath: post.imageUrl,
+            };
+          }),
           totalPosts: resData.totalItems,
           postsLoading: false,
         });
@@ -107,7 +110,6 @@ class Feed extends Component {
     this.setState({
       editLoading: true,
     });
-    // Set up data (with image!)
     const formData = new FormData();
     formData.append("title", postData.title);
     formData.append("content", postData.content);
@@ -115,9 +117,10 @@ class Feed extends Component {
     let url = "http://localhost:8080/feed/post";
     let method = "POST";
     if (this.state.editPost) {
-      url = "URL";
+      url = "http://localhost:8080/feed/post/" + this.state.editPost._id;
+      method = "PUT";
     }
-    console.log(postData);
+
     fetch(url, {
       method: method,
       body: formData,
@@ -129,6 +132,7 @@ class Feed extends Component {
         return res.json();
       })
       .then((resData) => {
+        console.log(resData);
         const post = {
           _id: resData.post._id,
           title: resData.post.title,
@@ -171,7 +175,7 @@ class Feed extends Component {
 
   deletePostHandler = (postId) => {
     this.setState({ postsLoading: true });
-    fetch("URL")
+    fetch("http://localhost:8080/feed/post/" + postId, { method: "DELETE" })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Deleting a post failed!");
